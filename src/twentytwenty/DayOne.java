1ddc6f19b38dcc087292197fff88662001f2b792
Tobias Bjerking <tobias.bjerking@gmail.com>
2020-11-30 22:40:47 +0100

    add file for tomorrow

---
+++ src/twentytwenty/DayOne.java
@@ -0,0 +1,21 @@
+package twentytwenty;
+
+import static shared.Input.*;
+import static shared.Utils.*;
+
+public class DayOne {
+
+    private static void partOne(){
+        return;
+    }
+
+    private static void partTwo(){
+        return;
+    }
+
+    public static void main(String[] args) {
+        String input = readRaw("src\\twentynineteen\\DayOne.txt");
+        print(input);
+
+    }
+}
