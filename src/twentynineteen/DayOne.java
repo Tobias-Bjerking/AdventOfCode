ea446766435c2bfa0ffa1542e72202d6f592bb2d
Tobias Bjerking <tobias.bjerking@gmail.com>
2020-11-30 22:41:18 +0100

    day 1 of 2019

---
+++ src/twentynineteen/DayOne.java
@@ -0,0 +1,40 @@
+package twentynineteen;
+
+import java.util.List;
+
+import static shared.Input.*;
+import static shared.Utils.*;
+
+public class DayOne {
+
+    private static int calculateFuel(int mass){
+        return mass / 3 -2;
+    }
+
+    private static int partOne(List<Integer> input){
+        int total = 0;
+        for (int module : input)
+            total += calculateFuel(module);
+        return total;
+    }
+
+    private static int partTwo(List<Integer> input){
+        int total = 0;
+        for (int module : input) {
+            int fuel = module;
+            fuel = calculateFuel(fuel);
+            while (fuel > 0) {
+                total += fuel;
+                fuel = calculateFuel(fuel);
+            }
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> input = readAsIntList("src\\twentynineteen\\DayOne.txt");
+        print(partOne(input));
+        print(partTwo(input));
+
+    }
+}
