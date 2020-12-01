dd4553cf1c0bf28f385c3c0039a60c297e10098d
Tobias Bjerking <tobias.bjerking@gmail.com>
2020-12-01 06:22:32 +0100

    add file for day 2

---
+++ src/twentytwenty/DayTwo.java
@@ -0,0 +1,23 @@
+package twentytwenty;
+
+import java.util.List;
+
+import static shared.Input.readAsIntList;
+import static shared.Utils.print;
+
+public class DayTwo {
+
+    private static int partOne(List<Integer> input) {
+        return -1;
+    }
+
+    private static int partTwo(List<Integer> input) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> input = readAsIntList("src\\twentytwenty\\DayTwo.txt");
+        print(partOne(input));
+//        print(partTwo(input));
+    }
+}
