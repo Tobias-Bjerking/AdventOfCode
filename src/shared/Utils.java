8a281221b6ca1b26d96ec626cba740774aa40061
Tobias Bjerking <tobias.bjerking@gmail.com>
2020-12-01 18:57:40 +0100

    add timer functionality to utils

--- src/shared/Utils.java
+++ src/shared/Utils.java
@@ -1,11 +1,24 @@
 package shared;
 
+import java.time.LocalDateTime;
+
 public class Utils {
+    static int time;
+
+
     public static void print(String s) {
         System.out.println(s);
     }
 
     public static void print(int s) {
         System.out.println(s);
+    }
+
+    public static void start() {
+        time = LocalDateTime.now().getNano();
+    }
+
+    public static void stop() {
+        print("Time: " + String.valueOf(LocalDateTime.now().getNano() - time % 1000) + " ms\n");
     }
 }
