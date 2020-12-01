44bdea81402c78f9a6e51987c5d6337bb6d58872
Tobias Bjerking <tobias.bjerking@gmail.com>
2020-12-01 06:20:30 +0100

    day 1 2020

--- src/twentytwenty/DayOne.java
+++ src/twentytwenty/DayOne.java
@@ -1,21 +1,32 @@
 package twentytwenty;
 
+import java.util.List;
+
 import static shared.Input.*;
 import static shared.Utils.*;
 
 public class DayOne {
 
-    private static void partOne(){
-        return;
+    private static int partOne(List<Integer> input) {
+        for (int x = 0; x < input.size(); x++)
+            for (int y = 0; y < input.size(); y++)
+                if (input.get(x) + input.get(y) == 2020)
+                    return input.get(x) * input.get(y);
+        return -1;
     }
 
-    private static void partTwo(){
-        return;
+    private static int partTwo(List<Integer> input) {
+        for (int x = 0; x < input.size(); x++)
+            for (int y = 0; y < input.size(); y++)
+                for (int z = 0; z < input.size(); z++)
+                    if (input.get(x) + input.get(y) + input.get(z)== 2020)
+                    return input.get(x) * input.get(y) * input.get(z);
+        return -1;
     }
 
     public static void main(String[] args) {
-        String input = readRaw("src\\twentynineteen\\DayOne.txt");
-        print(input);
-
+        List<Integer> input = readAsIntList("src\\twentytwenty\\DayOne.txt");
+        print(partOne(input));
+        print(partTwo(input));
     }
 }
