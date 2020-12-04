51288139266d6db7db8b3ce835bfae842b9f51f4
Tobias Bjerking <tobias.bjerking@gmail.com>
2020-12-05 00:17:46 +0100

    adding files for day 5

---
+++ src/twentytwenty/DayFive.java
@@ -0,0 +1,25 @@
+package twentytwenty;
+
+import java.io.IOException;
+import java.util.List;
+
+import static shared.Input.*;
+import static shared.Utils.*;
+
+public class DayFive {
+
+    private static int partOne(List<Integer> input){
+        return -1;
+    }
+
+    private static int partTwo(List<Integer> input){
+        return -1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<Integer> input = readAsIntList("src\\twentynineteen\\DayFive.txt");
+        print(partOne(input));
+        print(partTwo(input));
+
+    }
+}
