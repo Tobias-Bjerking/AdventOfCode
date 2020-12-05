6bdfa008040b50528e31a1531306080cdb5ae00e
Tobias Bjerking <tobias.bjerking@gmail.com>
2020-12-05 13:36:16 +0100

    Day 5 2020

--- src/twentytwenty/DayFive.java
+++ src/twentytwenty/DayFive.java
@@ -1,25 +1,81 @@
 package twentytwenty;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import static shared.Input.*;
-import static shared.Utils.*;
+import static shared.Input.readAsList;
+import static shared.Utils.print;
 
 public class DayFive {
 
-    private static int partOne(List<Integer> input){
-        return -1;
+    private static int calculateID(Seat seat) { return seat.getRow() * 8 + seat.getColumn(); }
+
+    private static int partOne(List<String> input){
+        int highestID = -1;
+        for (String s : input) {
+            Seat seat = parse(s);
+            int id = calculateID(seat);
+            if (id > highestID)
+                highestID = id;
+        }
+        return highestID;
     }
 
-    private static int partTwo(List<Integer> input){
-        return -1;
+    private static int partTwo(List<String> input){
+        HashMap<Integer, ArrayList<Seat>> rows = new HashMap<>();
+
+        for (String s : input) {
+            Seat seat = parse(s);
+            List<Seat> row = rows.computeIfAbsent(seat.getRow(), k -> new ArrayList<>());
+            row.add(seat);
+        }
+
+        List<Seat> incompleteRow = null;
+        for (ArrayList<Seat> row : rows.values()) {
+            if (row.size() == 7)
+                incompleteRow = row;
+        }
+
+        int total = (incompleteRow.size() + 1) * (incompleteRow.size() + 2) / 2;
+        for (int i = 0; i < incompleteRow.size(); i++) {
+            total -= incompleteRow.get(i).getColumn() + 1;
+        }
+
+        return calculateID(new Seat(incompleteRow.get(0).getRow(), total - 1));
     }
 
-    public static void main(String[] args) throws IOException {
-        List<Integer> input = readAsIntList("src\\twentynineteen\\DayFive.txt");
+    private static Seat parse(String boardingCode) {
+        int row = 0;
+        int column = 0;
+        for(int i = 0; i < boardingCode.length(); i++) {
+            char c = boardingCode.charAt(boardingCode.length()-i-1);
+            if (c == 'B' || c == 'F') {
+                row += c == 'B' ? Math.pow(2, i-3)  : 0;
+            }
+            else
+                column += c == 'R' ? Math.pow(2, i) : 0;
+        }
+        return new Seat(row, column);
+    }
+
+    public static void main(String[] args) {
+        List<String> input = readAsList("src\\twentytwenty\\input\\DayFive.txt");
         print(partOne(input));
         print(partTwo(input));
+    }
 
+    static class Seat {
+        int row;
+        int column;
+
+        public Seat(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int getRow() { return row; }
+
+        public int getColumn() { return column; }
     }
 }
